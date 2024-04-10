a15454df308c15b717bd5a0d0101ca730349fbe1
st4pzz <ramellajr@gmail.com>
2024-04-10 19:47:27 -0300

    jogador final final

--- src/main/java/insper/store/jogador/JogadorOut.java
+++ src/main/java/insper/store/jogador/JogadorOut.java
@@ -5,14 +5,13 @@ import lombok.experimental.Accessors;
 @Builder
 @Accessors(fluent = true, chain = true)
 public record JogadorOut(
-    String id,
+    String id_jogador,
     String id_user,
     String id_partida,
     String nome,
     String time,
     String posicao,
-    Integer idade,
-    Float salario
+    Integer idade
 ) {
     
 }
